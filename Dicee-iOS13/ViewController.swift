28d445982a2060dd328099f9c95b28e752286752
valdo112 <68995931+valdo112@users.noreply.github.com>
2023-05-29 19:31:09 +0700

    Add files via upload

---
+++ Dicee-iOS13/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Dicee-iOS13
+//
+//  Created by Angela Yu on 11/06/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var diceImageView2: UIImageView!
+    @IBOutlet weak var diceImageView1: UIImageView!
+    var leftDiceNumber = 0
+    var rightDiceNumber = 5
+    
+    var diceNumber = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+
+    @IBAction func rollButtonView(_ sender: UIButton) {
+        
+        
+        
+        diceImageView1.image = diceNumber [Int.random(in: 0...5)]
+        
+        diceImageView2.image = diceNumber [Int.random(in: 0...5)]
+        //we can also use randomElement() instead of Int.random(in:)
+        
+        
+    }
+    
+}
+
